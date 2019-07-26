0a812e139e992d6b8f9b429d1a831c592d93203f
GeraldBaratoux <geraldbaratoux@gmail.com>
2019-07-26 16:02:59 +0200

    Modif notes

--- ProjetIntermediaire/notes.py
+++ ProjetIntermediaire/notes.py
@@ -1,5 +1,6 @@
 import sys, json
-from PySide2.QtWidgets import QApplication, QWidget
+import numpy as np
+from PySide2.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetItem, QDoubleSpinBox
 from notes_ui import Ui_Form
 
 
@@ -14,6 +15,10 @@ class MainWindow(QWidget):
 
         self.ui.cbAcademie.currentIndexChanged.connect(self.updateEtablissements)
         self.ui.cbEtablissement.currentIndexChanged.connect(self.updateClasse)
+        self.ui.cbClasse.currentIndexChanged.connect(self.updateMatiere)
+        self.ui.cbMatiere.currentIndexChanged.connect(self.updateSaisieEleve)
+
+
 
         self.updateAcademies()
 
@@ -31,6 +36,47 @@ class MainWindow(QWidget):
         self.ui.cbClasse.clear()
         for a in self.dico["academies"][self.ui.cbAcademie.currentIndex()]["etablissements"][self.ui.cbEtablissement.currentIndex()]["classes"]:
             self.ui.cbClasse.addItem(a["nom"])
+
+    def updateMatiere(self):
+        dicoClasse = self.dico["academies"][self.ui.cbAcademie.currentIndex()]["etablissements"][self.ui.cbEtablissement.currentIndex()]["classes"][self.ui.cbClasse.currentIndex()]
+        listeMatieres = []
+        for eleve in dicoClasse["eleves"]:
+            for matiere in eleve["matieres"]:
+                listeMatieres.append(matiere["nom"])
+
+        listeMatieresUniques = np.unique(listeMatieres)
+        self.ui.cbMatiere.addItems(listeMatieresUniques)
+
+
+
+
+
+    def updateSaisieEleve(self):
+        cpt = 0
+        self.ui.twNotes.clear()
+        self.ui.twNotes.setColumnCount(2)
+        dicoClasse = self.dico["academies"][self.ui.cbAcademie.currentIndex()]["etablissements"][self.ui.cbEtablissement.currentIndex()]["classes"][self.ui.cbClasse.currentIndex()]
+        for eleve in dicoClasse["eleves"]:
+            for matiere in eleve["matieres"]:
+                mat = self.ui.cbMatiere.currentText()
+                if matiere["nom"] == mat:
+                    nomE = eleve["nom"]
+                    self.ui.twNotes.setRowCount(cpt+1)
+                    itemE = QTableWidgetItem(nomE)
+                    self.ui.twNotes.setItem(cpt, 0, itemE)
+                    spinB = QDoubleSpinBox()
+                    spinB.setProperty("nom", nomE)
+                    self.ui.twNotes.setCellWidget(cpt,1,spinB)
+                    cpt = cpt+1
+
+        self.ui.twNotes.setHorizontalHeaderLabels(['Nom', 'Note'])
+
+
+
+
+
+
+
 
 
         #self.ui.twNotes.horizontalHeaderItem(0).setText(QApplication.translate("Form", "gggg", None, -1))
