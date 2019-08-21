75d164e7603aec5fe23ff3c357618b4d4205e3ea
GeraldBaratoux <geraldbaratoux@gmail.com>
2019-08-21 10:06:16 +0200

    Creation ConnectBDD

---
+++ Flask/connectBDD.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template, g
+import sqlite3
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+def connect_db():
+    sql = sqlite3.connect('/mnt/c/Users/antho/Documents/data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    session.pop('name', None)
+    return '<h1>Hello, World!</h1>'
+
+if __name__ == '__main__':
+    app.run()
+
